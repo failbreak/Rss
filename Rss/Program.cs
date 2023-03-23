2bf764f5c191deb6d772d38875b70f6092e88197
Empire Works <Empire Works@DESKTOP-U7R6FSG>
2023-03-23 19:47:30 +0100

    Add project files.

---
+++ Rss/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+XDocument.Load("https://www.dr.dk/nyheder/service/feeds/senestenyt").Root
+    .Element("channel")
+    .Elements("item")
+    .Select(x => new
+    {
+        Title = x.Element("title").Value,
+        Link = x.Element("link").Value,
+        Dato = x.Element("pubDate").Value
+    })
+    .ToList()
+    .ForEach(x => { Console.WriteLine($"\n {x.Title} \n \t :{x.Link}: \n \t \t {x.Dato} \n"); });
